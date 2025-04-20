468d1c444e50f76ff8387cdec9680969056016fb
LuisCampelo2 <luisfilipecab@gmail.com>
2025-04-20 20:45:22 -0300

    primeiro commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,16 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partner } = person;
+
+  return (
+    <div className="Person">
+      <div className="Person__name">{name}</div>
+      {age && <div className="Person__age">{age}</div>}
+      <div className="Person__partner">
+        {isMarried
+          ? `My ${sex === 'm' ? 'wife' : 'husband'} is ${partner}`
+          : 'I am not married'}
+      </div>
+    </div>
+  );
+};
+

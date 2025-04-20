f3e68e4aaf707f181f16ffd8fc6174cd88cbf39d
LuisCampelo2 <luisfilipecab@gmail.com>
2025-04-20 20:57:23 -0300

    segundo commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,24 @@
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partner } = person;
+  const { name, age, sex, isMarried, partnerName } = person;
 
   return (
     <div className="Person">
-      <div className="Person__name">{name}</div>
-      {age && <div className="Person__age">{age}</div>}
-      <div className="Person__partner">
-        {isMarried
-          ? `My ${sex === 'm' ? 'wife' : 'husband'} is ${partner}`
-          : 'I am not married'}
+      <div className="Person__name">
+        {name}
       </div>
+
+      {age && (
+        <div className="Person__age">
+          {age}
+        </div>
+      )}
+
+    <div className="Person__partner">
+      {isMarried
+        ? `My ${sex === 'm' ? 'wife' : 'husband'} is ${partnerName}`
+        : 'I am not married'}
+      </div>;
     </div>
+
   );
 };
-
